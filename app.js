108fd8d872dfbad71c9c2352772debf46678c07d
1pyke <liethzaitoun99@gmail.com>
2021-12-23 17:34:45 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard= false;
+let lockBoard = false
+let firstCard,secondCard;
+
+function flipcard(){
+    if (lockBoard) return;
+    if (this === firstCard)return;
+
+   this.classList.add('flip');
+
+   if(!hasFlippedCard){
+
+       hasFlippedCard= true
+       firstCard=this
+
+       return;
+   }
+       secondCard=this
+
+        checkForMatch()             
+   }
+   function checkForMatch() {
+       let isMatch=firstCard.dataset.framework === 
+       secondCard.dataset.framework;
+       isMatch ? disableCards() : unflipCards();
+   }
+
+function disableCards(){
+    firstCard.removeEventListener ('click',flipcard); 
+    secondCard.removeEventListener ('click',flipcard);
+    restBoard()
+}
+
+function unflipCards(){
+    lockBoard = true;
+
+    setTimeout(() =>{
+        firstCard.classList.remove ('flip') ;
+        secondCard.classList.remove('flip');
+
+        restBoard();
+    }, 1500)
+}
+function restBoard(){
+    [hasFlippedCard, lockBoard] = [false,false] ;
+    [firstCard , secondCard] = [null, null] ;
+}
+(function shuffle(){
+   cards.forEach(card => {
+       let randomPos = Math.floor(Math.random()*12)
+       card.style.order = randomPos
+   }); 
+})();
+
+cards.forEach(card=> card.addEventListener('click', flipcard));
